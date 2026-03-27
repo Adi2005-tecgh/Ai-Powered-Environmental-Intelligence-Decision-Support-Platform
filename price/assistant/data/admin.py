3d208966c4af3dc9a39d8152f0089933b72414be
Adi2005-tecgh <adityasonakanalli123@gmail.com>
2026-03-27 13:57:37 +0530

    Delete price/assistant directory

--- price/assistant/data/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Product, PriceHistory, UserPreference
-
-admin.site.register(Product)
-admin.site.register(PriceHistory)
-admin.site.register(UserPreference)
--- price/assistant/data/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import product_list, product_detail, price_history, update_prices
-
-urlpatterns = [
-    path('', product_list, name='product_list'),  # List all products
-    path('<int:product_id>/', product_detail, name='product_detail'),
-    path('<int:product_id>/history/', price_history, name='price_history'),
-    path('update-prices/', update_prices, name='update_prices'),
-]
