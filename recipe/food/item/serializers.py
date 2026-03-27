d9624a3e46a3ac2ae85bef0aac222a305aa615d5
Adi2005-tecgh <adityasonakanalli123@gmail.com>
2026-03-27 13:57:57 +0530

    Delete recipe/food directory

--- recipe/food/item/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Recipe
-
-class recipeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Recipe
-        fields="__all__"
-
--- recipe/food/item/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Recipe
-from .serializers import recipeSerializer
-from rest_framework import viewsets
-
-
-class RecipeViewSet(viewsets.ModelViewSet):  # ✅ Use ViewSet instead of generic view
-    queryset = Recipe.objects.all()
-    serializer_class = recipeSerializer
